faad03dd41ffc3a73f4c4814ad8b1ef28b4cb62f
Arkeopix <mendiej750@gmail.com>
2014-10-02 12:36:37 +0200

    mojo::redis

--- web-service/test.pl
+++ web-service/test.pl
@@ -1,10 +1,17 @@
 use Mojolicious::Lite;
-use Mojo::UserAgent;
+use Mojo::Redis;
 
-my $ua = Mojo::UserAgent->new;
+my $redis = Mojo::Redis->new( server => '127.0.0.1:6379' );
+
+get '/' => sub {
+  my ( $self ) = @_;
+
+  $self->render( text => "coucou" );
+};
 
 get '/:foo' => sub {
-  my $self = shift;
+  my ( $self ) = @_;
+
   my $foo = $self->param( 'foo' );
   $self->render( text => "hello from $foo" );
 };

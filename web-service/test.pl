060e3904ece5a9a0a79cc2e2a15c9e95caa4e576
Arkeopix <mendiej750@gmail.com>
2014-10-08 15:07:39 +0200

    db works lol

--- web-service/test.pl
+++ web-service/test.pl
@@ -1,19 +1,31 @@
 use Mojolicious::Lite;
 use Mojo::Redis;
+use DBI;
 
 my $redis = Mojo::Redis->new( server => '127.0.0.1:6379' );
 
 get '/' => sub {
   my ( $self ) = @_;
 
-  $self->render( text => "coucou" );
+  my $dsn = 'DBI:mysql:database=wtm;host=127.0.0.1;port=3306';
+  my $user = 'root';
+  my $pass = '';
+  my $dbh =  DBI->connect( $dsn, $user, $pass, { RaiseError => 1} )
+    || die "Couldn't connect to database: " . DBI->errstr;
+
+  my $sth = $dbh->prepare( 'SELECT pseudo_user FROM users' );
+  $sth->execute;
+
+  my $tmp = $sth->fetchrow_arrayref;
+  my $user_pseudo = @$tmp[0];
+  $self->render( text => $user_pseudo );
 };
 
-get '/:foo' => sub {
+get '/:method/:req' => sub {
   my ( $self ) = @_;
 
-  my $foo = $self->param( 'foo' );
-  $self->render( text => "hello from $foo" );
+  my $method = $self->param( 'method' );
+  $self->render( text => "$method method" );
 };
 
 app->start;

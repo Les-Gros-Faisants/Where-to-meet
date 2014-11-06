8e85ca8ec8ec7dada8464d228626a2c2a017af1f
Arkeopix <mendiej750@gmail.com>
2014-11-06 14:09:00 +0100

    lol

--- web_service/lib/WebService/Controller/Insert.pm
+++ web_service/lib/WebService/Controller/Insert.pm
@@ -9,16 +9,75 @@ sub add_user {
   my $self = shift;
 
   my $json = decode_json( $self->req->body );
-  my $users = $self->db->resultset( 'User' );
+  my $user = $self->db->resultset( 'User' );
   my $ret = $user->update_or_create( {
     passwd_user => $json->{ passwd },
-    pseudo_user => $json->{ username }
-  });
+    pseudo_user => $json->{ username },
+  } );
   return $self->render( text => $ret->id_user );
 }
 
 sub update_user {
   my $self = shift;
-  
+
+  my $id = $self->param( 'id' );
+  my $json = decode_json( $self->req->body );
+  my $user = $self->db->resultset( 'User' );
+  my $ret = $user->update_or_create( {
+    id_user => $id,
+    passwd_user => $json->{ passwd },
+    pseudo_user => $json->{ username },
+  } );
+  $log->debug( $ret );
+  return $self->render( text => 'ok' );
 }
+
+sub update_user_passwd {
+  my $self = shift;
+
+  my $id = $self->param( 'id' );
+  my $json = decode_json( $self->req->body );
+  my $user = $self->db->resultset( 'User' )->find( {
+    id_user => $id
+  } );
+  my $ret = $user->update( {
+    password_user => $json->{ passwd }
+  } );
+  return $self->render( text => 'ok' );
+}
+
+sub update_user_pseudo {
+  my $self = shift;
+
+  my $id = $self->param( 'id' );
+  my $json = decode_json( $self->req->body );
+  my $user = $self->db->resultset( 'User' )->find( {
+    id_user => $id
+  } );
+  my $ret = $user->update( {
+    pseudo_user => $json->{ username }
+  } );
+  return $self->render( text => 'ok' );
+}
+
+sub add_tag {
+  my $self = shift;
+
+  my $json = decode_json( $self->req->body );
+  my $tag = $self->db->resultset( 'Tag' );
+  my $ret = $tag->create( {
+    tag_name => $json->{ tag_name },
+    id_victim => $json->{ id_victim },
+    id_aggressor => $json->{ id_aggressor },
+  });
+  return $self->render( text => 'ok' );
+}
+
+sub add_event {
+  my $self = shift;
+
+  my $json = decode_json( $self->req->body );
+  my $event = $self->db->resultset( 'PastEvent' );
+}
+
 1;

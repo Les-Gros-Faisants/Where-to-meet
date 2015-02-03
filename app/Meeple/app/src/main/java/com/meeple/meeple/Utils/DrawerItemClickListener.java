e3a439d32e7a0f7e8c66545f46b9d74cca5149fd
Sarevok117 <vuilla_l@epitech.eu>
2015-02-03 12:45:17 +0100

    Android studio project creation

---
+++ app/Meeple/app/src/main/java/com/meeple/meeple/Utils/DrawerItemClickListener.java
@@ -0,0 +1,10 @@
+package com.meeple.meeple.Utils;
+
+import android.widget.ListView;
+
+/**
+ * Created by Vuilla_l on 03/02/2015.
+ */
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+}

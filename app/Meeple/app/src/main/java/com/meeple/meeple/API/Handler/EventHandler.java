4450ff7ef3c390f85419d035a8ef36478c57231c
Arkeopix <mendiej750@gmail.com>
2015-02-12 12:33:30 +0100

    Event Handler

--- app/Meeple/app/src/main/java/com/meeple/meeple/API/Handler/EventHandler.java
+++ app/Meeple/app/src/main/java/com/meeple/meeple/API/Handler/EventHandler.java
@@ -3,6 +3,7 @@ package com.meeple.meeple.API.Handler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.meeple.meeple.Fragments.EventFragment;
 import org.apache.http.Header;
+import org.json.JSONException;
 import org.json.JSONObject;
 import android.util.Log;
 import com.meeple.meeple.Models.Event;
@@ -20,7 +21,22 @@ public class EventHandler extends JsonHttpResponseHandler {
     public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
         if (statusCode == 200) {
             Log.i("Got: ", response.toString());
-            this._frag.getEventSuccess(new Event());
+            try {
+                this._frag.getEventSuccess(new Event(response.getInt("id_event"),
+                        response.getInt("id_organizer"),
+                        response.getString("geolocation"),
+                        response.getString("description"),
+                        response.getString("event_name"),
+                        response.getString("date_event")));
+
+            }
+            catch (JSONException e) {
+                Log.e("Something wnt wrong: ", e.getMessage());
+                this._frag.getEventFailure(e.getMessage());
+            }
+        }
+        else {
+            this._frag.getEventFailure("Server returned: " + statusCode);
         }
     }
 }

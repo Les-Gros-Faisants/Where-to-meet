6819ff94eac9664a811e981a646106f9c13c390f
Arkeopix <mendiej750@gmail.com>
2015-02-16 16:55:42 +0100

    add db_grunt.pl

---
+++ technical_stuff/db_grunt.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+use DBI;
+
+$SIG{INT}  = \&clean_exit;
+$SIG{KILL} = \&clean_exit;
+
+my $dbh = DBI->connect( 'dbi:mysql:database=wtm:host=localhost',
+    'root', 'Pangea/poil.21' )
+  || die "Can't connect to db: $DBI::errstr";
+
+sub clean_exit {
+    $dbh->disconnect
+      || die "Disconnection error: $DBI::errstr\n";
+}
+
+while (42) {
+    my $sth = $dbh->do(
+        'UPDATE wtm SET active=false WHERE now() - date_event > timeout');
+    sleep(5);
+}

ac2ba9a87b308bf77823b5d4941404593bf42664
Arkeopix <mendiej750@gmail.com>
2015-02-16 13:30:07 +0100

    update createEvent

--- app/Meeple/app/src/main/java/com/meeple/meeple/API/Handler/CreateEventHandler.java
+++ app/Meeple/app/src/main/java/com/meeple/meeple/API/Handler/CreateEventHandler.java
@@ -1,10 +1,38 @@
 package com.meeple.meeple.API.Handler;
 
+import android.util.Log;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.meeple.meeple.Fragments.EventCreationFragment;
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by arkeopix on 2/16/15.
  */
 public class CreateEventHandler extends JsonHttpResponseHandler {
+    private EventCreationFragment _frag;
 
+    public CreateEventHandler() {}
+    public CreateEventHandler(EventCreationFragment frag) {
+        this._frag = frag;
+    }
+
+    @Override
+    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+        try {
+            if (statusCode == 200 && response.getString("ret").equals("OK")) {
+                this._frag.eventCreationSuccess();
+            }
+        }
+        catch (JSONException e) {
+            Log.e("error: ", e.getMessage());
+            this._frag.eventCreationFailure(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+        this._frag.eventCreationFailure(e.getMessage());
+    }
 }

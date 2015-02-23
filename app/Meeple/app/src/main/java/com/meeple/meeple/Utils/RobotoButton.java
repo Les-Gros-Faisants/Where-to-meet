cc68ef98f0961e4910ad08ca4126d57aa9f76efe
Sarevok117 <Sarevok117@hotmail.fr>
2015-02-23 22:01:38 +0100

    integration

--- app/Meeple/app/src/main/java/com/meeple/meeple/Utils/RobotoButton.java
+++ app/Meeple/app/src/main/java/com/meeple/meeple/Utils/RobotoButton.java
@@ -3,30 +3,32 @@ package com.meeple.meeple.Utils;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import android.widget.Button;
 
 /**
- * Created by Vuilla_l on 22/02/2015.
+ * Created by Vuilla_l on 23/02/2015.
  */
-public class RobotoTextView extends TextView {
-    public RobotoTextView(Context context) {
+public class RobotoButton extends Button {
+
+    public RobotoButton(Context context) {
         super(context);
         setFont();
     }
 
-    public RobotoTextView(Context context, AttributeSet attrs) {
+    public RobotoButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         setFont();
     }
 
-    public RobotoTextView(Context context, AttributeSet attrs, int defStyle) {
+    public RobotoButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         setFont();
     }
 
     private void setFont() {
         Typeface font = Typeface.createFromAsset(getContext().getAssets(),
-                "fonts/RobotoSlab-Light.ttf");
+                "fonts/RobotoSlab-Regular.ttf");
         setTypeface(font);
+        setTextColor(0xffdbdbdb);
     }
 }
